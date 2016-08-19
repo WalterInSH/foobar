6032881201cbd1a9bff84624ab3361748354e032
Walter <walterinsh@icloud.com>
2016-08-19 17:00:43 +0800

    solve rabbit test

---
+++ rabbit_test_subjects/solution.java
@@ -0,0 +1,24 @@
+    public static int answer(double[] y, double[] x) {
+        double ysum = sum(y);
+        double xsum = sum(x);
+
+        if (xsum > ysum) {
+            return cal(xsum, ysum);
+        } else if (xsum < ysum) {
+            return cal(ysum, xsum);
+        } else {
+            return 0;
+        }
+    }
+
+    private static int cal(double before, double after) {
+        return (int) ((before - after) * 100 / before);
+    }
+
+    private static double sum(double[] numbers) {
+        double r = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            r += numbers[i];
+        }
+        return r;
+    }
